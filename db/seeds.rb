3d9f9213113efcc00f49dd82406d4853c8f85d85
Nicolas Saddi <saddi.nicolas@gmail.com>
2018-03-28 17:16:23 -0300

    Yelp

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Restaurant.destroy_all
+
+5.times do
+  Restaurant.create(name: Faker::Book.title, address: Faker::Address.street_address, category: ["chinese", "italian", "japanese", "french", "belgian"].sample, phone_number: Faker::PhoneNumber.cell_phone)
+end
+
